11230147718a60633800b43b330ace7764546f88
sebinside <hi@sebinside.de>
2017-10-06 17:01:02 +0200

    Added figure creation

---
+++ bundles/de.cooperateproject.cdo.dawn.frontend.draw2d/web_content/shapes/View.js
@@ -0,0 +1,19 @@
+View = draw2d.Canvas.extend({
+
+    init:function(id)
+    {
+        this._super(id);
+
+    },
+
+    onDrop : function(droppedDomNode, x, y, shiftKey, ctrlKey)
+    {
+        var type = $(droppedDomNode).data("shape");
+        var figure = eval("new "+type+"();");
+
+        // create a command for the undo/redo support
+        var command = new draw2d.command.CommandAdd(this, figure, x, y);
+        this.getCommandStack().execute(command);
+    }
+});
+
